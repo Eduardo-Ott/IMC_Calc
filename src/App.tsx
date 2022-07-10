b8ca0b53f10b0159a5babe7d77990b8299c7165a
Eduardo Ott <eduardovott@gmail.com>
2022-07-10 19:07:08 -0300

    Ajustando a responsividade

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,96 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from 'react';
+import styles from './App.module.css';
+import poweredImage from './assets/powered.png';
+import leftArrowImage from './assets/leftarrow.png';
+import { GridItem } from './components/GridItem';
 
-function App() {
-  const [count, setCount] = useState(0)
+import {levels, calculateImc, Level} from './helpers/imc'
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+const App = () => {
+
+
+  const [heightField, setHeightField] = useState<number>(0);
+  const [weightField, setWeightField] = useState<number>(0);
+  const [toShow, setToShow] = useState<Level|null>(null);
+
+  const handleCalculateButton = () => {
+    if(heightField && weightField) {
+      setToShow(calculateImc(heightField, weightField));
+    }else {
+      alert("Digite todos os campos!")
+    }
+  }
+
+  const handleBackButton = () => {
+    setToShow(null);
+    setHeightField(0);
+    setWeightField(0);
+  }
+
+  return(
+    <div className={styles.main}>
+
+      <header>
+        <div className={styles.headerContainer}>
+          <img src={poweredImage} alt="" width={150} />
+        </div>
       </header>
+      <div className={styles.container}>
+
+      <div className={styles.leftside}>
+
+          <h1> Calcule o seu IMC </h1>
+          
+          <p>
+          IMC é a sigla para Índice de Massa Corpórea, parâmetro adotado pela Organização Mundial de Saúde para calcular o peso ideal de cada pessoa.
+          </p>
+
+          <input
+            type="number"
+            placeholder="Digite a sua altura. Ex: 1.5 (Em metros)"
+            value={heightField > 0 ? heightField : ''}
+            onChange={e => setHeightField(parseFloat(e.target.value))}
+            disabled={toShow ? true : false}
+          />
+
+          <input
+            type="number"
+            placeholder="Digite o seu peso. Ex: 75.3 (Em Kg)"
+            value={weightField > 0 ? weightField : ''}
+            onChange={e => setWeightField(parseFloat(e.target.value))}
+            disabled={toShow ? true : false}
+          />
+
+          <button onClick={handleCalculateButton} disabled={toShow ? true : false}>
+            Calcular
+          </button>
+
+      </div>
+      <div className={styles.rightside}>
+          {!toShow &&
+            <div className={styles.grid}>
+              {levels.map((item, key) => (
+                <GridItem key={key} item={item}/>
+              ))}
+            </div> 
+          }
+          {toShow &&
+            <div className={styles.rightBig}>
+              <div className={styles.rightArrow} onClick={handleBackButton}>
+                <img src={leftArrowImage} alt="" width="25px"/>
+              </div>
+
+              <GridItem item={toShow}/>           
+            </div>
+          }
+      </div>
+
+      </div>
+
     </div>
-  )
+   );
+
 }
 
-export default App
+
+export default App;
